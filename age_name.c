9f50f83ca8acbbacdef61bcf6ca82b6153523904
catchmilan <53039622+catchmilan@users.noreply.github.com>
2020-05-14 08:19:11 +0545

    Add files via upload

---
+++ age_name.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main()
+{
+    char c,name[100];
+    int age,i;
+    printf("Your name is:\n");
+    for(i=0; (c = getchar())!='\n'; i++)
+        name[i]=c;
+    name[i]='\0';
+    printf("Enter the age:");
+    scanf("%d",&age);
+    printf("Hello %s, next year you will be %d.",name, age+1);
+    return 0;
+}
